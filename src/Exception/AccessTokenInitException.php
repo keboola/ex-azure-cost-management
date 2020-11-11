9f61de8e680b8337027555811cb1a0400ad47d88
Michal Jurečko <michal.jurecko@gmail.com>
2020-11-11 17:07:23 +0100

    composer require guzzlehttp/guzzle

---
+++ src/Exception/AccessTokenInitException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AzureCostExtractor\Exception;
+
+use Keboola\CommonExceptions\ApplicationExceptionInterface;
+
+class AccessTokenInitException extends \Exception implements ApplicationExceptionInterface
+{
+
+}
